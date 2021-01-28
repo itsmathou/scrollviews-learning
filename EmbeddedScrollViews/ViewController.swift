ee00afc33570f5e7294a59f5c68cf56d12ebaf53
Mathilde Ferrand <mathilde.ferrand.coeurderoy@gmail.com>
2021-01-28 18:44:55 +0000

    initial commit

---
+++ EmbeddedScrollViews/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EmbeddedScrollViews
+//
+//  Created by Mathilde Ferrand on 28/01/2021.
+//
+
+import UIKit
+
+final class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemRed
+    }
+
+
+}
+
